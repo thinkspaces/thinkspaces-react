7863c7ead3d85a02e55621c77a0bdd66901f05ce
Sarim Abbas <sarim.stays@gmail.com>
2019-02-19 13:12:04 -0500

    added createdTimestamp to signup
    Need this for the Firestore -> mailchimp integration

--- src/firebase/db.js
+++ src/firebase/db.js
@@ -114,7 +114,8 @@ export const createUserwithFields = async (uid, profileData) => {
     .collection("users")
     .doc(uid)
     .set({
-      ...profileData
+      ...profileData,
+      createdTimestamp : createTimestamp(new Date())
     });
 };
 
