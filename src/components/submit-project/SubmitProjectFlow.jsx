92ab049880e6afff5205c01d747e673cf49a388e
Felicia Chang <felicia.chang@yale.edu>
2019-04-28 22:22:13 -0700

    progress bar

--- src/components/submit-project/SubmitProjectFlow.jsx
+++ src/components/submit-project/SubmitProjectFlow.jsx
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
-import { Button, FormGroup, Label, Input, Form } from 'reactstrap';
+import { Row, Col, Button, FormGroup, Label, Input, Form } from 'reactstrap';
 import withAuthorization from '../utils/withAuthorization';
 import { db } from '../../firebase';
 import GeneralProjectInfo from './components/general-project-info';
 import ProjectDetails from './components/project-details';
 import ProjectRequest from './components/project-request';
+import './SubmitProjectFlow.css';
 
 const _locations = [
   { label: 'Yale' },
@@ -43,6 +44,23 @@ const _skills = [
   { label: 'Management' },
 ];
 const _commitments = [ { label: 'High' }, { label: 'Medium' }, { label: 'Low' } ];
+
+// const ProgressBar = props => (
+//   <div className="progress-bar">
+//     <Filler step={this.props.step} />
+//   </div>
+// );
+//
+// const Filler = (props) => {
+//   switch (this.props.step) {
+//     case 1:
+//       return <div className="filler" style={{ width: '${33}%' }} />;
+//     case 2:
+//       return <div className="filler" style={{ width: '${66}%' }} />;
+//     case 3:
+//       return <div className="filler" style={{ width: '${100}%' }} />;
+//   }
+// };
 
 class SubmitProjectFlow extends Component {
   state = { step: 1,
@@ -105,7 +123,15 @@ class SubmitProjectFlow extends Component {
     switch (step) {
       case 1:
         return (
-          <div>
+          <div className="formStyle">
+            <Row>
+              <p style={{ paddingLeft: 17, paddingRight: 30 }}> Step 1/3 </p>
+
+              <div className="progressbar">
+                <div className="filler" style={{ width: '33%' }} />
+              </div>
+            </Row>
+            <br />
             <GeneralProjectInfo
               title={title}
               links={links}
@@ -116,24 +142,56 @@ class SubmitProjectFlow extends Component {
               onChangeCardDes={event => this.setState({ card_des: event.target.value })}
               onChangeContact={event => this.setState({ contact: event.target.value })}
             />
-            <Button onClick={event => this.setState({ step: 2 })}> Next </Button>
+            <Button
+              className="nextButton"
+              color="primary"
+              outline="true"
+              onClick={event => this.setState({ step: 2 })}
+            >
+              Next
+            </Button>
           </div>
         );
       case 2:
         return (
-          <div>
+          <div className="formStyle">
+            <Row>
+              <p style={{ paddingLeft: 17, paddingRight: 30 }}> Step 2/3 </p>
+
+              <div className="progressbar">
+                <div className="filler" style={{ width: '66%' }} />
+              </div>
+            </Row>
+            <br />
             <ProjectDetails
               about={about}
               onChangeAbout={event => this.setState({ about: event.target.value })}
               types={types}
               toggleItem={this.toggleItem}
             />
-            <Button onClick={event => this.setState({ step: 3 })}> Next </Button>
+            <div onClick={event => this.setState({ step: 1 })}>Back</div>
+            <Button
+              className="nextButton"
+              color="primary"
+              outline="true"
+              onClick={event => this.setState({ step: 3 })}
+            >
+              {' '}
+              Next{' '}
+            </Button>
           </div>
         );
       case 3:
         return (
-          <div>
+          <div className="formStyle">
+            <Row>
+              <p style={{ paddingLeft: 17, paddingRight: 30 }}> Step 3/3 </p>
+              <br />
+              <div className="progressbar">
+                <div className="filler" style={{ width: '100%' }} />
+              </div>
+            </Row>
+
             <ProjectRequest
               need={need}
               onChangeNeed={event => this.setState({ need: event.target.value })}
@@ -141,7 +199,11 @@ class SubmitProjectFlow extends Component {
               commitment={commitments}
               toggleItem={this.toggleItem}
             />
-            <Button onClick={this.onSubmit}> Submit </Button>
+            <div onClick={event => this.setState({ step: 2 })}>Back</div>
+            <Button className="nextButton" color="primary" outline="true" onClick={this.onSubmit}>
+              {' '}
+              Submit{' '}
+            </Button>
           </div>
         );
     }
@@ -150,7 +212,8 @@ class SubmitProjectFlow extends Component {
   render() {
     return (
       <div>
-        <h2> Submit a Project </h2>
+        <h2 className="titleStyle"> Create a Project </h2>
+        <br />
         {this.renderSwitch(this.state.step)}
       </div>
     );
